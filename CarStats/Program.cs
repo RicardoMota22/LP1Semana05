979f15be8efd5c1a6190de5c39dafe13cc95a9b5
RicardoMota22 <ricardomota.artist@gmail.com>
2025-03-21 17:40:50 +0000

    Exercicio de Avaliação, Add Seed

---
+++ CarStats/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Spectre.Console;
+using Bogus;
+using System.Security.Cryptography;
+
+namespace CarStats
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Randomizer.Seed = new Random(int.Parse(args[0]));
+            Faker faker = new Faker();
+            int random = Randomizer.Number(20);
+
+            BarChart bc = new BarChart();
+            //bc.AddItem("Ferrari",14);
+
+            
+            
+            bc.AddItem(faker,random);
+            Console.WriteLine(bc);
+            
+        }
+    }
+}
